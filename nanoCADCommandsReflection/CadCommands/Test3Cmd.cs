7b2dfa33e785c884295ac236abb2bd7a088f2996
kpblc2000 <kpblc2000@gmail.com>
2024-10-29 00:05:58 +0300

    Ввод команды без описания

---
+++ nanoCADCommandsReflection/CadCommands/Test3Cmd.cs
@@ -0,0 +1,15 @@
+﻿using nanoCADCommandsReflection.Infrasructure;
+using Teigha.Runtime;
+
+namespace nanoCADCommandsReflection.CadCommands
+{
+    public static class Test3Cmd
+    {
+        [CommandMethod("Test3")]
+        public static void Test3Command()
+        {
+            MessageService msgService = new MessageService();
+            msgService.InfoMessage(nameof(Test3Command));
+        }
+    }
+}
